66db3cfa44dab25b4631b72abbf8aabfd8c525a1
mgred <mgred@users.noreply.github.com>
2022-04-15 17:32:19 +0000

    deploy: 96cc4692e8987b807e19b189bfe05550cfce954d

--- .github/actions/build-page/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-action_dir=`dirname ${BASH_SOURCE[0]}`
-
-pandoc README.md -s -o index.html \
-  --metadata pagetitle="althttpd docker" \
-  --include-in-header=$action_dir/header.html
